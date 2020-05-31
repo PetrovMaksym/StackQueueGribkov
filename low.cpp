a593f9ee0aa0991794f3e75d1a56e8018be37c36
PetrovMaksym <61542889+PetrovMaksym@users.noreply.github.com>
2020-05-31 18:32:26 +0300

    Update low.cpp

--- low.cpp
+++ low.cpp
@@ -1,12 +1,127 @@
 #include "Stdafx.h"
+#include "low.h"
+
 using namespace std;
+
 /*
-Создать очередь, информационными полями которого являются: компьютер и объем его оперативной памяти.
-Удалить из очереди сведения о компьютере, которые были введены первыми. Организовать просмотр данных
-очереди и вычислить общий объем памяти компьютеров, записанных в очереди
+РЎРѕР·РґР°С‚СЊ РѕС‡РµСЂРµРґСЊ РёР· РІРµС‰РµСЃС‚РІРµРЅРЅС‹С… С‡РёСЃРµР». РћРїСЂРµРґРµР»РёС‚СЊ РјРёРЅРёРјР°Р»СЊРЅС‹Р№ СЌР»РµРјРµРЅС‚ РѕС‡РµСЂРµРґРё. РћСЂРіР°РЅРёР·РѕРІР°С‚СЊ РїСЂРѕСЃРјРѕС‚СЂ 
+РґР°РЅРЅС‹С… РѕС‡РµСЂРµРґРё
 */
+
 void low()
 {
 	system("cls");
-	cout << "\nLow task level:\n";
+	cout << "\nLow level task: \n";
+	queue* q;
+	q = (queue*)malloc(sizeof(queue*));
+	init(q);
+	int count = 10 + rand() % 20;
+	cout << "\nРљРѕР»РёС‡РµСЃС‚РІРѕ Р·РЅР°С‡РµРЅРёР№ РѕС‡РµСЂРµРґРё: " << count <<" Р—РЅР°С‡РµРЅРёСЏ РѕС‡РµСЂРµРґРё: \n";
+	for (size_t i = 0; i < count; i++)
+	{
+		insert(q, 0.3 + (rand() % 100) / 12.1);
+	}
+	print(q);
+	double min = min_elem(q);
+	printf("\nРњРёРЅРёРјР°Р»СЊРЅС‹Р№ СЌР»РµРјРµРЅС‚ РѕС‡РµСЂРµРґРё -> %1.1f\n", min); 
+	cout << "РќР°Р¶РјРёС‚Рµ Enter РґР»СЏ Р·Р°РІРµСЂС€РµРЅРёСЏ...";
+	while(getchar() != '\n')
+		continue;
 }
+struct list
+{
+	double digit;
+	list* next;
+};
+
+struct queue
+{
+	list* front, * tail;
+};
+double min_elem(queue* q)
+{
+	list* h;
+	if (isempty(q) == 1) {
+		printf("РћС‡РµСЂРµРґСЊ РїСѓСЃС‚Р°!\n");
+		return 0.0;
+	}
+	double min = 1000.0;
+	for (h = q->front; h != nullptr; h = h->next)
+		if (h->digit < min)
+			min = h->digit;
+	return min;
+}
+double remove(queue* q)
+{
+	struct list* temp;
+	double digit;
+	if (isempty(q) == 1) {
+		printf("РћС‡РµСЂРµРґСЊ РїСѓСЃС‚Р°!\n");
+		return 0;
+	}
+	digit = q->front->digit;
+	temp = q->front;
+	q->front = q->front->next;
+	free(temp);
+	return digit;
+}
+
+void print(queue* q)
+{
+	list* h;
+	if (isempty(q) == 1) {
+		printf("РћС‡РµСЂРµРґСЊ РїСѓСЃС‚Р°!\n");
+		return;
+	}
+	for (h = q->front; h != nullptr; h = h->next)
+		printf("%1.1f ", h->digit);
+	return;
+}
+
+
+void init(queue* q)
+{
+	q->front = nullptr;
+	q->tail = nullptr;
+}
+
+int isempty(queue* q)
+{
+	if (q->front == nullptr)
+		return 1;
+	return 0;
+}
+
+void insert(queue* q, double digit)
+{
+	if ((q->tail == nullptr) && (q->front == nullptr)) {
+		q->tail = push_back(digit);
+		q->front = q->tail;
+	}
+	else
+		q->tail = addelem(q->tail, digit);
+}
+
+list* addelem(list* lst, double digit)
+{
+	list* temp, * p;
+	temp = (list*)malloc(sizeof(list));
+	p = lst->next; // СЃРѕС…СЂР°РЅРµРЅРёРµ СѓРєР°Р·Р°С‚РµР»СЏ РЅР° СЃР»РµРґСѓСЋС‰РёР№ СѓР·РµР»
+	lst->next = temp; // РїСЂРµРґС‹РґСѓС‰РёР№ СѓР·РµР» СѓРєР°Р·С‹РІР°РµС‚ РЅР° СЃРѕР·РґР°РІР°РµРјС‹Р№
+	temp->digit = digit; // СЃРѕС…СЂР°РЅРµРЅРёРµ РїРѕР»СЏ РґР°РЅРЅС‹С… РґРѕР±Р°РІР»СЏРµРјРѕРіРѕ СѓР·Р»Р°
+	temp->next = p; // СЃРѕР·РґР°РЅРЅС‹Р№ СѓР·РµР» СѓРєР°Р·С‹РІР°РµС‚ РЅР° СЃР»РµРґСѓСЋС‰РёР№ СЌР»РµРјРµРЅС‚
+	return(temp);
+}
+
+list* push_back(double digit)
+{
+	list* lst;
+	// РІС‹РґРµР»РµРЅРёРµ РїР°РјСЏС‚Рё РїРѕРґ РєРѕСЂРµРЅСЊ СЃРїРёСЃРєР°
+	lst = (list*)malloc(sizeof(list));
+	lst->digit = digit;
+	lst->next = nullptr; // СЌС‚Рѕ РїРѕСЃР»РµРґРЅРёР№ СѓР·РµР» СЃРїРёСЃРєР°
+	return(lst);
+}
+
+
+
